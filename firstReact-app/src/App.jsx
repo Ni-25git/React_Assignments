e977946a67a38ae8fc20baf3c82cfd98d7721a07
Ni-25git <nipunsherawat@gmail.com>
2023-11-03 11:09:32 +0530

    First React app

---
+++ firstReact-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <h1>This is my first React project</h1>
+      <button style={{backgroundColor: "red"}}>Click me</button>
+     </div>
+    </>
+  )
+}
+
+export default App
